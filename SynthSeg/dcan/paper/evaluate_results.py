f8b2bc5583c57ca675aff06fc01ae6e6b085ff0a
paul-reiners <paul.reiners@gmail.com>
2022-02-08 13:33:26 -0600

    Check to make sure folder isn't empty.

--- SynthSeg/dcan/paper/evaluate_results.py
+++ SynthSeg/dcan/paper/evaluate_results.py
@@ -14,14 +14,17 @@ def evaluate_results():
         gt_dir = os.path.join(nnunet_dir, f'raw_data/Task516_525/gt_labels/Fold{i}/')
         seg_dir = os.path.join(nnunet_dir, f'segmentations/inferred/PaperCrossValidation/Task{516 + i}_Paper_Fold{i}/')
         if os.path.exists(seg_dir):
-            summary_dir = os.path.join(result_dir, f'fold{i}')
-            path_hausdorff = os.path.join(summary_dir, 'hausdorff/hausdorff')
-            path_hausdorff_99 = os.path.join(summary_dir, 'hausdorff_99/hausdorff_99')
-            path_hausdorff_95 = os.path.join(summary_dir, 'hausdorff_95/hausdorff_95')
-            path_mean_distance = os.path.join(summary_dir, 'mean_distance/mean_distance')
-            path_dice = os.path.join(summary_dir, 'dice/dice')
-            evaluation(gt_dir,
-                       seg_dir,
-                       label_list, path_hausdorff=path_hausdorff, path_hausdorff_99=path_hausdorff_99,
-                       path_hausdorff_95=path_hausdorff_95, path_mean_distance=path_mean_distance, path_dice=path_dice,
-                       summary_dir=summary_dir)
+            dir_list = os.listdir(seg_dir)
+            if len(dir_list) != 0:
+                summary_dir = os.path.join(result_dir, f'fold{i}')
+                path_hausdorff = os.path.join(summary_dir, 'hausdorff/hausdorff')
+                path_hausdorff_99 = os.path.join(summary_dir, 'hausdorff_99/hausdorff_99')
+                path_hausdorff_95 = os.path.join(summary_dir, 'hausdorff_95/hausdorff_95')
+                path_mean_distance = os.path.join(summary_dir, 'mean_distance/mean_distance')
+                path_dice = os.path.join(summary_dir, 'dice/dice')
+                evaluation(gt_dir,
+                           seg_dir,
+                           label_list, path_hausdorff=path_hausdorff, path_hausdorff_99=path_hausdorff_99,
+                           path_hausdorff_95=path_hausdorff_95, path_mean_distance=path_mean_distance,
+                           path_dice=path_dice,
+                           summary_dir=summary_dir)
