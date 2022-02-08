230cfb783f24defed5e5912e73860fd7f23a3f62
paul-reiners <paul.reiners@gmail.com>
2022-02-08 14:31:58 -0600

    Concatenate folds.

---
+++ SynthSeg/dcan/paper/append_fold_files.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+from os.path import exists
+
+
+def append_fold_files(results_dir):
+    measures = ['dice', 'hausdorff', 'hausdorff_95', 'hausdorff_99', 'mean_distance']
+    for measure in measures:
+        df = None
+        folds = ['fold{}'.format(i) for i in range(10)]
+        for fold in folds:
+            data_file_path = os.path.join(results_dir, fold, measure, f'{measure}.csv')
+            if exists(data_file_path):
+                next_df = pd.read_csv(data_file_path)
+                if df is not None:
+                    df = pd.concat([df, next_df], axis=0, ignore_index=True)
+                else:
+                    df = next_df
+        df.to_csv(os.path.join(results_dir, f'{measure}.csv'), index=False)
--- SynthSeg/dcan/paper/generate_metrics.py
+++ SynthSeg/dcan/paper/generate_metrics.py
@@ -1,9 +1,10 @@
 # Author: Paul Reiners
-
+from SynthSeg.dcan.paper.append_fold_files import append_fold_files
 from SynthSeg.dcan.paper.evaluate_results import evaluate_results
 from SynthSeg.dcan.paper.generate_metrics_csv_files import generate_metrics_csv_files
 
 if __name__ == "__main__":
     results_dir = '/home/feczk001/shared/data/nnUNet/segmentations/inferred/PaperCrossValidation/results/'
-    evaluate_results(results_dir)
-    generate_metrics_csv_files(results_dir)
+    # evaluate_results(results_dir)
+    # generate_metrics_csv_files(results_dir)
+    append_fold_files(results_dir)
