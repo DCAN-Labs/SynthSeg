be650627f3d69957e8632add446b5f866d67d253
paul-reiners <paul.reiners@gmail.com>
2022-02-09 16:43:45 -0600

    Made plots horizontal..

--- SynthSeg/dcan/paper/create_plots.py
+++ SynthSeg/dcan/paper/create_plots.py
@@ -9,10 +9,10 @@ def create_violin_plots(results_dir, measures):
     # https://mode.com/blog/violin-plot-examples/
     for measure in measures:
         data_file_path = os.path.join(results_dir, f'{measure}.csv')
-        df = pd.read_csv(data_file_path, index_col='subject')
+        df = pd.read_csv(data_file_path)
         cols = df.columns.tolist()
         cols = cols[:1] + [str(col) for col in sorted([int(col) for col in cols[1:]])]
         df = df[cols]
         sns.set(style='whitegrid')
-        sns.violinplot(data=df)
+        sns.violinplot(data=df, orient='h')
         plt.savefig(f'../../../img/paper/{measure}.png')
