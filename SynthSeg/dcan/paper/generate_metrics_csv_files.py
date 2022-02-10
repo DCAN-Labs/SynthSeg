dde4be1859937a933a13fe06e39f7db480c94677
paul-reiners <paul.reiners@gmail.com>
2022-02-10 10:04:51 -0600

    Use region names rather than numbers.

--- SynthSeg/dcan/paper/generate_metrics_csv_files.py
+++ SynthSeg/dcan/paper/generate_metrics_csv_files.py
@@ -4,8 +4,10 @@ import os
 import numpy as np
 import pandas as pd
 
+from SynthSeg.dcan.look_up_tables import get_id_to_region_mapping
 
-def generate_metrics_csv_files(results_dir, measures):
+
+def generate_metrics_csv_files(results_dir, measures, mapping_file_name, alternate_mapping_file_name):
     folds = ['fold{}'.format(i) for i in range(10)]
     for fold in folds:
         for measure in measures:
@@ -14,13 +16,28 @@ def generate_metrics_csv_files(results_dir, measures):
                 labels_file_path = os.path.join(results_dir, fold, 'labels.txt')
                 with open(labels_file_path) as fp:
                     lines = fp.readlines()
-                    labels = [int(line.strip()) for line in lines]
+                    labels = sorted([int(line.strip()) for line in lines])
                 path_segs_path = os.path.join(results_dir, fold, 'path_segs.txt')
                 with open(path_segs_path) as fp:
                     lines = fp.readlines()
                     paths_segs = [line.strip() for line in lines]
                 data = np.load(data_file_path)
                 transposed_data = data.transpose()
-                df = pd.DataFrame(transposed_data, columns=labels, index=paths_segs)
+                id_to_region = get_id_to_region_mapping(mapping_file_name, separator=None)
+                alternate_id_to_region = get_id_to_region_mapping(alternate_mapping_file_name, separator=None)
+                for item in alternate_id_to_region.items():
+                    identifier = item[0]
+                    if identifier not in id_to_region.keys():
+                        region = item[1]
+                        id_to_region[identifier] = region
+                regions = [id_to_region[label] for label in labels]
+                df = pd.DataFrame(transposed_data, columns=regions, index=paths_segs)
                 csv_file_path = os.path.join(results_dir, fold, measure, f'{measure}.csv')
                 df.to_csv(csv_file_path, index_label='subject')
+
+if __name__ == "__main__":
+    results_folder = '/home/feczk001/shared/data/nnUNet/segmentations/inferred/PaperCrossValidation/results/'
+    msrs = ['dice', 'hausdorff', 'hausdorff_95', 'hausdorff_99', 'mean_distance']
+    mapping_file = '../../../data/labels_classes_priors/dcan/Freesurfer_LUT_DCAN.md'
+    alternate_mapping_file = '../../../data/labels_classes_priors/dcan/FreeSurferColorLUT.txt'
+    generate_metrics_csv_files(results_folder, msrs, mapping_file, alternate_mapping_file)
