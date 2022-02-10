4f819da4109ad2cdf33a35e8fe9e614093cdf6cb
paul-reiners <paul.reiners@gmail.com>
2022-02-10 13:30:14 -0600

    Remove columns with zeros

--- SynthSeg/dcan/paper/create_plots.py
+++ SynthSeg/dcan/paper/create_plots.py
@@ -10,6 +10,14 @@ def create_violin_plots(results_dir, measures):
     for measure in measures:
         data_file_path = os.path.join(results_dir, f'{measure}.csv')
         df = pd.read_csv(data_file_path)
+        df.drop('Unknown', axis=1, inplace=True)
+        # Remove columns with zeros
+        df = df.loc[:, (df != 0).all(axis=0)]
         sns.set(style='whitegrid')
         sns.violinplot(data=df, orient='h')
         plt.savefig(f'../../../img/paper/{measure}.png')
+
+if __name__ == "__main__":
+    results_folder = '/home/feczk001/shared/data/nnUNet/segmentations/inferred/PaperCrossValidation/results/'
+    msrs = ['dice', 'hausdorff', 'hausdorff_95', 'hausdorff_99', 'mean_distance']
+    create_violin_plots(results_folder, msrs)
