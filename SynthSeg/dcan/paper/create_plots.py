a1b28e0cc5c63662d49adae9048b104e65d02d87
paul-reiners <paul.reiners@gmail.com>
2022-02-10 14:02:17 -0600

    Using three kinds of plot.

--- SynthSeg/dcan/paper/create_plots.py
+++ SynthSeg/dcan/paper/create_plots.py
@@ -16,10 +16,42 @@ def create_violin_plots(results_dir, measures):
             # 0 seems to mean NaN to SynthSeg
             df = df.loc[:, (df != 0).all(axis=0)]
         sns.set(style='whitegrid')
+        sns.violinplot(data=df, orient='h')
+        plt.savefig(f'../../../img/paper/violinplot/{measure}.png')
+
+
+def create_box_plots(results_dir, measures):
+    # https://mode.com/blog/violin-plot-examples/
+    for measure in measures:
+        data_file_path = os.path.join(results_dir, f'{measure}.csv')
+        df = pd.read_csv(data_file_path)
+        df.drop('Unknown', axis=1, inplace=True)
+        if measure == 'dice':
+            # Remove columns with zeros
+            # 0 seems to mean NaN to SynthSeg
+            df = df.loc[:, (df != 0).all(axis=0)]
+        sns.set(style='whitegrid')
         sns.boxplot(data=df, orient='h')
-        plt.savefig(f'../../../img/paper/{measure}.png')
+        plt.savefig(f'../../../img/paper/boxplot/{measure}.png')
+
+
+def create_cat_plots(results_dir, measures):
+    # https://mode.com/blog/violin-plot-examples/
+    for measure in measures:
+        data_file_path = os.path.join(results_dir, f'{measure}.csv')
+        df = pd.read_csv(data_file_path)
+        df.drop('Unknown', axis=1, inplace=True)
+        if measure == 'dice':
+            # Remove columns with zeros
+            # 0 seems to mean NaN to SynthSeg
+            df = df.loc[:, (df != 0).all(axis=0)]
+        sns.set(style='whitegrid')
+        sns.catplot(data=df, orient='h')
+        plt.savefig(f'../../../img/paper/catplot/{measure}.png')
 
 if __name__ == "__main__":
     results_folder = '/home/feczk001/shared/data/nnUNet/segmentations/inferred/PaperCrossValidation/results/'
     msrs = ['dice', 'hausdorff', 'hausdorff_95', 'hausdorff_99', 'mean_distance']
     create_violin_plots(results_folder, msrs)
+    create_box_plots(results_folder, msrs)
+    create_cat_plots(results_folder, msrs)
