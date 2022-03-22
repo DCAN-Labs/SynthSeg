7ad8bebcbad99395703d4816fc5259fdac1e6630
paul-reiners <paul.reiners@gmail.com>
2022-03-22 12:04:42 -0500

    Clear the figure as needed.

---
+++ SynthSeg/dcan/paper/evaluate_results_by_age.py
@@ -0,0 +1,33 @@
+# Author: Paul Reiners
+
+import os.path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+if __name__ == "__main__":
+    chirality_corrected_folder = \
+        '/home/feczk001/shared/data/nnUNet/segmentations/inferred/PaperCrossValidation/chirality_corrected'
+    output_folder = os.path.join(chirality_corrected_folder, 'results_by_age/')
+    csv_file = os.path.join(chirality_corrected_folder, 'results/dice.csv')
+    dice_data_frame = pd.read_csv(csv_file)
+    dice_data_frame['age_in_months'] = dice_data_frame.apply(lambda row: row.subject[0], axis=1)
+    for age_in_months in range(9):
+       age_df = dice_data_frame[dice_data_frame["age_in_months"] == str(age_in_months)]
+       age_df.drop(['age_in_months'], axis=1, inplace=True)
+       sns.catplot(kind="box",
+                   data=age_df,
+                   height=4,
+                   aspect=1.5, orient='h')
+       sns.stripplot(data=age_df,
+                     alpha=0.3,
+                     jitter=0.2,
+                     color='k', orient='h')
+       fig = plt.gcf()
+       plt.ylabel("Region")
+       plt.title(f'Dice - {age_in_months}-months-old')
+       width = 7.0
+       height = 10.0
+       fig.set_size_inches(width, height)
+       plt.savefig(os.path.join(output_folder, f'dice_{age_in_months}-months-old.png'), dpi=100)
