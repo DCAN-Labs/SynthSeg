1dcdc0ef5594e8d85514cf7af4b98543ccc68ff9
LuciMoore <lucille.a.mo@gmail.com>
2022-05-09 11:56:06 -0700

    updated dilate_LRmask.py to integrate with Paul's code

--- SynthSeg/dcan/img_processing/chirality_correction/dilate_LRmask.py
+++ SynthSeg/dcan/img_processing/chirality_correction/dilate_LRmask.py
@@ -7,7 +7,7 @@ Arguments:
     sub_LRmask_dir: location of directory that contains the LRmask.nii.gz file generated for a subject
 
 Usage:
-  dilate_lr_mask  <sub_LRmask_dir>
+  dilate_lr_mask <input_LRmask> <output_dilated_LRmask>
   dilate_lr_mask -h | --help
 Options:
   -h --help     Show this screen.
@@ -17,75 +17,76 @@ import os
 import shutil
 import nibabel as nib
 import numpy as np
+import tempfile
 from docopt import docopt
 
 from nipype.interfaces import fsl
 
+def dilate_lr_mask(input_LRmask, output_dilated_LRmask):
+    root=os.path.dirname(input_LRmask)
+    td = tempfile.mkdtemp(prefix='{}/'.format(root))
 
-def dilate_lr_mask(sub_LRmask_dir):
-    os.chdir(sub_LRmask_dir)
-    if not os.path.exists('lrmask_dil_wd'):
-        os.mkdir('lrmask_dil_wd')
+    os.chdir(td)
 
-    anatfile = 'LRmask.nii.gz'
+    anatfile = input_LRmask
     maths = fsl.ImageMaths(in_file=anatfile, op_string='-thr 1 -uthr 1',
-                           out_file='lrmask_dil_wd/Lmask.nii.gz')
+                           out_file='Lmask.nii.gz')
     maths.run()
 
     maths = fsl.ImageMaths(in_file=anatfile, op_string='-thr 2 -uthr 2',
-                           out_file='lrmask_dil_wd/Rmask.nii.gz')
+                           out_file='Rmask.nii.gz')
     maths.run()
 
     maths.run()
     maths = fsl.ImageMaths(in_file=anatfile, op_string='-thr 3 -uthr 3',
-                           out_file='lrmask_dil_wd/Mmask.nii.gz')
+                           out_file='Mmask.nii.gz')
     maths.run()
 
     # dilate, fill, and erode each mask in order to get rid of holes
     # (also binarize L and M images in order to perform binary operations)
-    anatfile = 'lrmask_dil_wd/Lmask.nii.gz'
+    anatfile = 'Lmask.nii.gz'
     maths = fsl.ImageMaths(in_file=anatfile, op_string='-dilM -dilM -dilM -fillh -ero',
-                           out_file='lrmask_dil_wd/L_mask_holes_filled.nii.gz')
+                           out_file='L_mask_holes_filled.nii.gz')
     maths.run()
 
-    anatfile = 'lrmask_dil_wd/Rmask.nii.gz'
+    anatfile = 'Rmask.nii.gz'
     maths = fsl.ImageMaths(in_file=anatfile, op_string='-bin -dilM -dilM -dilM -fillh -ero',
-                           out_file='lrmask_dil_wd/R_mask_holes_filled.nii.gz')
+                           out_file='R_mask_holes_filled.nii.gz')
     maths.run()
 
-    anatfile = 'lrmask_dil_wd/Mmask.nii.gz'
+    anatfile = 'Mmask.nii.gz'
     maths = fsl.ImageMaths(in_file=anatfile, op_string='-bin -dilM -dilM -dilM -fillh -ero',
-                           out_file='lrmask_dil_wd/M_mask_holes_filled.nii.gz')
+                           out_file='M_mask_holes_filled.nii.gz')
     maths.run()
 
     # Reassign values of 2 and 3 to R and M masks (L mask already a value of 1)
-    anatfile = 'lrmask_dil_wd/R_mask_holes_filled.nii.gz'
+    anatfile = 'R_mask_holes_filled.nii.gz'
     maths = fsl.ImageMaths(in_file=anatfile, op_string='-mul 2',
-                           out_file='lrmask_dil_wd/R_mask_holes_filled_label2.nii.gz')
+                           out_file='R_mask_holes_filled_label2.nii.gz')
     maths.run()
 
-    anatfile = 'lrmask_dil_wd/M_mask_holes_filled.nii.gz'
+    anatfile = 'M_mask_holes_filled.nii.gz'
     maths = fsl.ImageMaths(in_file=anatfile, op_string='-mul 3',
-                           out_file='lrmask_dil_wd/M_mask_holes_filled_label3.nii.gz')
+                           out_file='M_mask_holes_filled_label3.nii.gz')
     maths.run()
 
     # recombine new L, R, and M mask files
-    anatfile_left = 'lrmask_dil_wd/L_mask_holes_filled.nii.gz'
-    anatfile_right = 'lrmask_dil_wd/R_mask_holes_filled_label2.nii.gz'
-    anatfile_mid = 'lrmask_dil_wd/M_mask_holes_filled_label3.nii.gz'
+    anatfile_left = 'L_mask_holes_filled.nii.gz'
+    anatfile_right = 'R_mask_holes_filled_label2.nii.gz'
+    anatfile_mid = 'M_mask_holes_filled_label3.nii.gz'
     maths = fsl.ImageMaths(in_file=anatfile_left, op_string='-add {}'.format(anatfile_right),
-                           out_file='lrmask_dil_wd/recombined_mask_LR.nii.gz')
+                           out_file='recombined_mask_LR.nii.gz')
     maths.run()
 
-    maths = fsl.ImageMaths(in_file=anatfile_mid, op_string='-add lrmask_dil_wd/recombined_mask_LR.nii.gz',
-                           out_file='lrmask_dil_wd/dilated_LRmask.nii.gz')
+    maths = fsl.ImageMaths(in_file=anatfile_mid, op_string='-add recombined_mask_LR.nii.gz',
+                           out_file='dilated_LRmask.nii.gz')
     maths.run()
 
     ## Fix incorrect values resulting from recombining dilated components
-    orig_LRmask_img = nib.load('LRmask.nii.gz')
+    orig_LRmask_img = nib.load(input_LRmask)
     orig_LRmask_data = orig_LRmask_img.get_fdata()
 
-    fill_LRmask_img = nib.load('lrmask_dil_wd/dilated_LRmask.nii.gz')
+    fill_LRmask_img = nib.load('dilated_LRmask.nii.gz')
     fill_LRmask_data = fill_LRmask_img.get_fdata()
 
     # Flatten numpy arrays
@@ -109,11 +110,11 @@ def dilate_lr_mask(sub_LRmask_dir):
     # save new numpy array as image
     empty_header = nib.Nifti1Header()
     out_img = nib.Nifti1Image(fill_LRmask_data_3D, orig_LRmask_img.affine, empty_header)
-    nib.save(out_img, 'LRmask_dil.nii.gz')
+    nib.save(out_img, output_dilated_LRmask)
 
     #remove working directory with intermediate outputs
-    #shutil.rmtree('lrmask_dil_wd')
+    shutil.rmtree(td)
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-    dilate_lr_mask(args['<sub_LRmask_dir>'])
+    dilate_lr_mask(args['<input_LRmask>'], args['<output_dilated_LRmask>'])
