2abe21162519139e9824ebe244bda6fca6b13af5
paul-reiners <paul.reiners@gmail.com>
2023-09-29 16:09:40 -0500

    Rewrote create_split.

--- SynthSeg/dcan/file_manipulation/split.py
+++ SynthSeg/dcan/file_manipulation/split.py
@@ -1,47 +1,63 @@
+import argparse
+import os
 import os.path
+import random
 from os import listdir
 from os.path import isfile, join
 import shutil
-import glob
-import os
-
-from sklearn.model_selection import ShuffleSplit
-
-my_path = '/home/feczk001/shared/data/nnUNet/nnUNet_raw_data_base/nnUNet_raw_data/Task550/'
-label_files = glob.glob(my_path + '[0-9]mo_SynthSeg_generated_[0-9][0-9][0-9][0-9].nii.gz')
-X = []
-groups = []
-for f in label_files:
-    file_name = os.path.basename(f)
-    X.append(f)
-    groups.append(file_name[0])
-rs = ShuffleSplit(n_splits=1, test_size=.20, random_state=0)
-
-base_dir = '/home/feczk001/shared/data/nnUNet/nnUNet_raw_data_base/nnUNet_raw_data/Task550/'
-labelsTs = '/home/feczk001/shared/data/nnUNet/labelsTs/550/'
-imagesTr = '/home/feczk001/shared/data/nnUNet/nnUNet_raw_data_base/nnUNet_raw_data/Task550/imagesTr/'
-imagesTs = '/home/feczk001/shared/data/nnUNet/nnUNet_raw_data_base/nnUNet_raw_data/Task550/imagesTs/'
-labelsTr = '/home/feczk001/shared/data/nnUNet/nnUNet_raw_data_base/nnUNet_raw_data/Task550/labelsTr/'
 
 
-def move_files(index, labels_dest, images_dest):
-    for i in index:
-        train_file = X[i]
-        src = os.path.join(base_dir, train_file)
-        dest = labels_dest
-        shutil.move(src, dest)
-
-        t1_file = train_file[:-7] + '_0000.nii.gz'
-        t1_src = t1_file
-        t1_dest = images_dest
-        shutil.move(t1_src, t1_dest)
-
-        t2_file = train_file[:-7] + '_0001.nii.gz'
-        t2_src = t2_file
-        t2_dest = images_dest
-        shutil.move(t2_src, t2_dest)
+def move_files(stems, src_labels_dir, dest_labels_folder, src_t1_dir, src_t2_dir, dest_images_folder):
+    for stem in stems:
+        label_file = f'{stem}.nii.gz'
+        label_file_path = os.path.join(src_labels_dir, label_file)
+        shutil.move(label_file_path, dest_labels_folder)
+        t1_file = f'{stem}_0000.nii.gz'
+        t1_file_path = os.path.join(src_t1_dir, t1_file)
+        shutil.move(t1_file_path, dest_images_folder)
+        t2_file = f'{stem}_0001.nii.gz'
+        t2_file_path = os.path.join(src_t2_dir, t2_file)
+        shutil.move(t2_file_path, dest_images_folder)
 
 
-for train_index, test_index in rs.split(X):
-    move_files(train_index, labelsTr, imagesTr)
-    move_files(test_index, labelsTs, imagesTs)
+def create_split(base_dir):
+    labels_ts = os.path.join(base_dir, 'labelsTs')
+    is_exist = os.path.exists(labels_ts)
+    if not is_exist:
+        os.makedirs(labels_ts)
+    images_tr = os.path.join(base_dir, 'imagesTr')
+    is_exist = os.path.exists(images_tr)
+    if not is_exist:
+        os.makedirs(images_tr)
+    images_ts = os.path.join(base_dir, 'imagesTs')
+    is_exist = os.path.exists(images_ts)
+    if not is_exist:
+        os.makedirs(images_ts)
+    labels_tr = os.path.join(base_dir, 'labelsTr')
+    is_exist = os.path.exists(labels_tr)
+    if not is_exist:
+        os.makedirs(labels_tr)
+
+    for age in range(9):
+        month_folder = os.path.join(base_dir, f'{str(age)}mo')
+        labels_dir = os.path.join(month_folder, 'labels')
+        age_label_files = [f for f in listdir(labels_dir) if isfile(join(labels_dir, f))]
+        file_count = len(age_label_files)
+        test_size = int(round(0.2 * file_count))
+        stems = list(map(lambda label_file_name: label_file_name[:-7], age_label_files))
+        test_stems = random.sample(stems, test_size)
+        train_stems = [stem for stem in stems if stem not in test_stems]
+        t1_dir = os.path.join(month_folder, 'T1w')
+        t2_dir = os.path.join(month_folder, 'T2w')
+        move_files(test_stems, labels_dir, labels_ts, t1_dir, t2_dir, images_ts)
+        move_files(train_stems, labels_dir, labels_tr, t1_dir, t2_dir, images_tr)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='split',
+        description='Splits items into training and test sets')
+    parser.add_argument('-b', '--base_dir')
+    args = parser.parse_args()
+    base_folder = args.base_dir
+    create_split(base_folder)
