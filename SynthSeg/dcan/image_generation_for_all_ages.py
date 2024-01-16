9490c2f9060b8f5f4afe5d1f0de7a76708e60d38
Paul Reiners <reine097@umn.edu>
2024-01-16 13:27:51 -0600

    Adding arg parsing.

--- SynthSeg/dcan/image_generation_for_all_ages.py
+++ SynthSeg/dcan/image_generation_for_all_ages.py
@@ -30,19 +30,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog='SynthSeg',
         description='Generates synthetic images from segmented images.',
-        epilog="Forked off of BBillot's SynthSeg")
+        epilog="Please contact reine097 for questions or problems.")
     parser.add_argument('input_dir', metavar='input-dir')
     parser.add_argument('output_dir', metavar='output-dir')
     parser.add_argument('min_max_file', metavar='min-mask-file')
     parser.add_argument(
-        'number_generated_images_per_real_image', metavar='number-generated-images-per-real-image', type=int)
+        'number_generated_images_per_age_group', metavar='number-generated-images-per-age-group', type=int)
     parser.add_argument('--starting-age-in-months', default=0, type=int)
     parser.add_argument('--distribution', default='normal',
                         help='distribution of priors (default: normal)', choices=['normal', 'uniform'])
     args = parser.parse_args()
     inpt_dr = args.input_dir
     output_dir = args.output_dir
-    n = args.number_generated_images_per_real_image
+    n = args.number_generated_images_per_age_group
     starting_age_in_months = args.starting_age_in_months
     distribution = args.distribution
     min_max_file = args.min_max_file
--- SynthSeg/dcan/ten_fold_uniformity_estimation.py
+++ SynthSeg/dcan/ten_fold_uniformity_estimation.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from tqdm import tqdm
@@ -5,6 +6,12 @@ from tqdm import tqdm
 from dcan.uniform_intensity_estimation_by_age import estimate_intensities_by_age
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='ten_fold_uniformity_estimation',
+        description='Creates SynthSeg uniform priors for ten-fold validation.',
+        epilog='Please contact reine097 for questions or problems.')
+    parser.parse_args()
+
     for i in tqdm(range(10), desc="age loop", position=0):
         task_dir = \
             f'/scratch.global/lundq163/nnUNet/nnUNet_raw_data_base/nnUNet_raw_data/Task{str(540 + i)}_T1_T2_Fold{i}/'
